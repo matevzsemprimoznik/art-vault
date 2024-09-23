490328818d1ff5567660d1a88fc08d1f7a80f2bf
Matevz <semprimoznik.matevz@gmail.com>
2024-09-23 12:47:09 +0200

    feat/client: add feature section on landing page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./app/**/*.{gjs,gts,hbs,html,js,ts}'],
   theme: {
     extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+      },
       colors: {
         tint: '#e67255',
         neutral: '#f4f5f7',
